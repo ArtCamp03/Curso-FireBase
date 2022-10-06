71ded42be44ca2297625f371ec47d0791f3304eb
Artur R Campos <arturrc10@outlook.com>
2022-10-06 14:17:20 -0300

    configurando permissoes

--- cursoFireBaseSF/app/src/main/java/br/arc_camp/firebaseactivity/MainActivity.kt
+++ cursoFireBaseSF/app/src/main/java/br/arc_camp/firebaseactivity/MainActivity.kt
@@ -1,6 +1,8 @@
 package br.arc_camp.firebaseactivity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -20,8 +22,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 
         binding.cadViewMainLogout.setOnClickListener(this)
+        binding.cadViewMainDownloadImg.setOnClickListener(this)
+        binding.cadViewMainUploadImg.setOnClickListener(this)
+        binding.cadViewMainLerDados.setOnClickListener(this)
+        binding.cadViewMainCategorias.setOnClickListener(this)
+        binding.cadViewMainGravarAlterarRemover.setOnClickListener(this)
 
         listenAuthentic()
+
+        // metodo de permissao para funcionalidades do celular
+        permission()
 
         supportActionBar?.hide()
     }
@@ -31,15 +41,64 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
             R.id.cadView_main_logout -> {
                 finish()
-
                 // desloga o usuario do Firebase
                 Firebase.auth.signOut()
-
                 startActivity(Intent(this, AberturaActivity::class.java))
+            }
 
+            R.id.cadView_main_download_img -> {
+                finish()
+                // desloga o usuario do Firebase
+                Firebase.auth.signOut()
+                startActivity(Intent(this, AberturaActivity::class.java))
+            }
+
+            R.id.cadView_main_upload_img -> {
+                finish()
+                // desloga o usuario do Firebase
+                Firebase.auth.signOut()
+                startActivity(Intent(this, AberturaActivity::class.java))
+            }
+
+            R.id.cadView_main_ler_dados -> {
+                finish()
+                // desloga o usuario do Firebase
+                Firebase.auth.signOut()
+                startActivity(Intent(this, AberturaActivity::class.java))
+            }
+
+            R.id.cadView_main_categorias -> {
+                finish()
+                // desloga o usuario do Firebase
+                Firebase.auth.signOut()
+                startActivity(Intent(this, AberturaActivity::class.java))
+            }
+
+            R.id.cadView_main_gravarAlterarRemover -> {
+                finish()
+                // desloga o usuario do Firebase
+                Firebase.auth.signOut()
+                startActivity(Intent(this, AberturaActivity::class.java))
             }
         }
+
     }
+
+    // feedback da resposta de permissoes
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        for(result in grantResults){
+            if(result == PackageManager.PERMISSION_DENIED){
+                Util.exibirToast(baseContext, "Aceite as permissoes para o app funcionar corretamente !!")
+                finish()
+                break
+            }
+        }
+
+    }
+
+
 
     // sempre escutara a respsota do Firebase
     private fun listenAuthentic(){
@@ -52,6 +111,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
 
         }
+    }
+
+    private fun permission(){
+        val permissoes = arrayOf<String>(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        )
+
+        Util.permissao(this, 100, permissoes)
+
     }
 
 }
